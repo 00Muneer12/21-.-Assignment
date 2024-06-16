2749cf4326bd252170084a023646404d37ea1c29
“[firstname <[gmugsk@gmail.com]>
2024-06-16 12:57:05 +0500

    21 Assignment

---
+++ main.js
@@ -0,0 +1,7 @@
+let book = {
+    title: "Hamood ur Rehman commission report",
+    author: "commission report",
+    yearPublished: 1973,
+};
+console.log(`Book Info: ${book.title} by ${book.author}, published in ${book.yearPublished}`);
+export {};
---
+++ main.ts
@@ -0,0 +1,6 @@
+let book: { title: string; author: string; yearPublished: number } = {
+    title: "Hamood ur Rehman commission report",
+    author: "commission report",
+    yearPublished: 1973,
+};
+console.log(`Book Info: ${book.title} by ${book.author}, published in ${book.yearPublished}`);
